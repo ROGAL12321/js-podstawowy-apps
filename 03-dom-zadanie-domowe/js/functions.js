e2b85bbb289f2712612206bd3cabdb13fd456895
Damian Rogulski <rogaldj@gmail.com>
2022-11-19 16:56:51 +0100

    Zajecia 19.11

---
+++ 03-dom-zadanie-domowe/js/functions.js
@@ -0,0 +1,17 @@
+// Zadania do zrobienia w domu z tematu funkcji
+
+// 1. Stworz tablice obiektów, zawierającą Twoje ulubione filmy. Kazdy film niech posiada takie wlasciwosci jak tytul, rok produkcji i ocena (number od 1 do 10). Nastepnie napisz program (bez uzycia funkcji), ktory wypisze w konsoli, ktore filmy maja ocene powyzej 8.
+
+// Przyklad w konsoli
+// Film Ojciec Chrzestny ma wg mnie ocene 10
+// Film Rozowa Pantera ma wg mnie ocene 9
+// Dla filmu z ocena 2, nie wyswietl nic
+
+// 2. Napisz funkcje multiply, ktora przyjmie dwie liczby a nastepnie wyswietli w konsoli wynik mnozenia
+
+// 3. Napisz funkcje o nazwie whoIsTaller, ktorej zadanie jest nastepujace:
+// - funkcja ma pobrac wzrost pierwszego uzytkownika
+// - funkcja ma pobrac wzrost drugiego uzytkownika
+// - funkcja ma wyswietlic informacje, kto jest wyzszy
+
+// 4. Napisz funkcje o nazwie findTheBestMovie, ktora przyjmie jako parametr liste Twoich ulubionych filmow i nastepnie zwroci napis "Moim ulubionym filmem jest Ojciec Chrzestny z ocena 10".
---
+++ 03-dom-zadanie-domowe/js/index.js
@@ -0,0 +1,12 @@
+// Zadanie domowe DOM
+
+// 1. Stworz w HTML formularz, oraz liste. Formularz niech zawiera 2 pola input: author oraz message. Zrob obsluge formularza w taki sposob, aby pod formularzem wyswietlic w liscie tekst
+
+// - Kurs ALX jest fajny - Damian
+// - Dzis jest brzydka pogoda - Agnieszka
+
+// 2. Pod formularzem, stworz przycisk Remove List, ktory po kliknieciu ma usunac wszystkie elementy listy.
+
+// 3*. Zrob walidacje formularza w taki sposob, zeby nie mozna bylo wyslac wiadomosci jesli ktorykolwiek z inputow jest pusty.
+
+// 4*. Ostyluj zadanie
